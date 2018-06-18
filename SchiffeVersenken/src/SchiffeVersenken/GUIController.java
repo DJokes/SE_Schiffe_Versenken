62f95c6d977a3ca479438cc6ae686f7eec13e22c
Jonas Sannewald <jonas.sannewald@gmail.com>
2018-06-18 23:56:01 +0200

    Fertigstellung des Controllers (GUI)

--- SchiffeVersenken/src/SchiffeVersenken/GUIController.java
+++ SchiffeVersenken/src/SchiffeVersenken/GUIController.java
@@ -2,10 +2,13 @@ package SchiffeVersenken;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import javax.swing.JButton;
+import javax.swing.JPanel;
 
-public class GUIController implements ActionListener {
+public class GUIController extends MouseAdapter implements ActionListener, MouseListener {
 	
 	private GUI gui = null;
 	private JButton opt1 = null;
@@ -13,6 +16,9 @@ public class GUIController implements ActionListener {
 	private JButton opt3 = null;
 	private JButton opt4 = null;
 	private JButton opt5 = null;
+	private JButton ship_placement_ok = null;
+	private JPanel[] leftGridFieldArray = null;
+	private JPanel[] rightGridFieldArray = null;
 	
 	public GUIController(GUI gui) {
 		this.gui = gui;
@@ -20,6 +26,7 @@ public class GUIController implements ActionListener {
 	}
 	
 	public void prepare() {
+		
 		opt1 = gui.getOpt1();
 		opt1.addActionListener(this);
 		
@@ -34,10 +41,23 @@ public class GUIController implements ActionListener {
 		
 		opt5 = gui.getOpt5();
 		opt5.addActionListener(this);
+		
+		ship_placement_ok = gui.getShip_placement_ok();
+		ship_placement_ok.addActionListener(this);
+		
+		leftGridFieldArray = gui.getLeftGridFieldArray();
+		for(int i = 0; i < leftGridFieldArray.length; i++)
+			leftGridFieldArray[i].addMouseListener(this);
+		
+		rightGridFieldArray = gui.getRightGridFieldArray();
+		for(int i = 0; i < leftGridFieldArray.length; i++)
+			rightGridFieldArray[i].addMouseListener(this);
+		
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		
 		if(e.getSource() == opt1) {
 			
 			 gui.removeRulesElements();
@@ -63,9 +83,36 @@ public class GUIController implements ActionListener {
 			gui.removeRulesFrame();
 			gui.openGameFrame();
 		
+		} else if(e.getSource() == ship_placement_ok) {
+			
+			gui.placeShip();
+			
 		} else {
 			System.exit(0);
 		}
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		
+		gui.changeGrid(0, 2, 0);
+        gui.changeGrid(1, 2, 99);
+		
+		for(int i = 0; i < leftGridFieldArray.length; i++) {
+			if(e.getSource() == leftGridFieldArray[i]) {
+				System.out.println("Linkes Spielfeld, Index: " + i);
+				break;
+			}
+		}
+		
+		for(int i = 0; i < rightGridFieldArray.length; i++) {
+			if(e.getSource() == rightGridFieldArray[i]) {
+				System.out.println("Rechtes Spielfeld, Index: " + i);
+				break;
+			}
+		}
+		
 	}
 
 }
