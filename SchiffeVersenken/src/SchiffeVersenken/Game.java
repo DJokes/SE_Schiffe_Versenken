4d19a47222b8996a6db380e40b95155ea7de3a04
Jens <Jens@Oeffler-PC>
2018-06-22 01:16:12 +0200

    Runden angepasst

--- SchiffeVersenken/src/SchiffeVersenken/Game.java
+++ SchiffeVersenken/src/SchiffeVersenken/Game.java
@@ -5,23 +5,36 @@ import java.awt.Color;
 public class Game {
 	private GUI gui = null;
 	private int hits = 30;
+	private boolean turn = determineBeginner();
 
 	public Game(GUI gui) {
 		this.gui = gui;
 
 	}
 
-	public int determineBeginner() {
-		return (int) (Math.random() * 2) + 1;
+	public boolean determineBeginner() {
+		int beginner = (int) (Math.random() * 2) + 1;
+		if (beginner == 1) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	public void shoot(int index) {
+
 		if (!isOver()) {
-			if (gui.getRightGridFieldArray()[index].getBackground() == Color.blue) {
-				gui.getRightGridFieldArray()[index].setBackground(Color.BLACK);
-			} else if (gui.getRightGridFieldArray()[index].getBackground() == Color.GREEN) {
-				gui.getRightGridFieldArray()[index].setBackground(Color.RED);
-				hits--;
+			if (turn) {
+				if (gui.getRightGridFieldArray()[index].getBackground() == Color.blue) {
+					gui.getRightGridFieldArray()[index].setBackground(Color.BLACK);
+					turn = false;
+				} else if (gui.getRightGridFieldArray()[index].getBackground() == Color.GREEN) {
+					gui.getRightGridFieldArray()[index].setBackground(Color.RED);
+					hits--;
+				}
+			}
+			else {
+				waitForEnemy();
 			}
 		}
 	}
@@ -35,7 +48,11 @@ public class Game {
 	}
 
 	public void waitForEnemy() {
-		// TODO
+		boolean enemyHasShot = false;
+		//TODO
+		if(enemyHasShot) {
+			turn = true;
+		}
 
 	}
 
