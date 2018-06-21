5b7fac459ce3196bec4f22ca10ae9ab1d0134a78
helenamr <helenamr@gmail.com>
2018-06-21 15:34:27 +0200

    small changes to Position
    >back to x and y
    >english names
    >up, down, left, right, cycle, add functions

--- SchiffeVersenken/src/SchiffeVersenken/Position.java
+++ SchiffeVersenken/src/SchiffeVersenken/Position.java
@@ -1,25 +1,64 @@
 package SchiffeVersenken;
 
-public class Position {
+public class Position {//doesn't check bounds
 
-	private int vertical;
-	private int horizontal;
+	private int x;
+	private int y;
 	
 	public Position(int horizontal, int vertical){
-		this.vertical = vertical;
-		this.horizontal = horizontal;
+		this.x = horizontal;
+		this.y = vertical;
 	}
 	
-	public int getVertical(){
-		return vertical;
+	public int getX(){
+		return this.x;
+	}
+	
+	public int getY(){
+		return this.y;
 	}
 	
 	public int getHorizontal(){
-		return horizontal;
+		return this.x;
 	}
 	
-	public void verschiebe(int deltaHorizontal, int deltaVertical){
-		vertical += deltaVertical;
-		horizontal += deltaHorizontal;
+	public int getVertical(){
+		return this.y;
+	}
+	
+	public Position move(int deltaHorizontal, int deltaVertical){
+		return new Position(this.x + deltaHorizontal, this.y + deltaVertical);
+	}
+	
+	public Position move(Position pos){
+		return this.add(pos);
+	}
+	
+	public Position add(Position pos){
+		return new Position(this.x + pos.getX(), this.y + pos.getY());
+	}
+	
+	public Position add(int value){//add a value to both x and y
+		return new Position(this.x + value, this.y + value);
+	}
+	
+	public Position up(){//position above
+		return new Position(this.x, this.y + 1);
+	}
+	
+	public Position right(){//position to the right
+		return new Position(this.x + 1, this.y);
+	}
+	
+	public Position down(){//position below
+		return new Position(this.x, this.y - 1);
+	}
+	
+	public Position left(){//position to the left
+		return new Position(this.x - 1, this.y);
+	}
+	
+	public Position cycle(){//position at the beginning of next line
+		return new Position(0, this.y + 1);
 	}
 }
