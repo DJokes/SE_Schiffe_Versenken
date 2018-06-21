82601253acd5054f67495af0dd18037981c4c3db
helenamr <helenamr@gmail.com>
2018-06-21 16:28:26 +0200

    New functions to Spielfeld
    constructors
    getTile
    setTile
    checkBounds

--- SchiffeVersenken/src/SchiffeVersenken/Spielfeld.java
+++ SchiffeVersenken/src/SchiffeVersenken/Spielfeld.java
@@ -4,45 +4,109 @@ public class Spielfeld {
 	
 	// 0 = water // 1 to 10 are ships their length // 100 is hit
 	private int[][] field = new int[10][10];
+	private int maxX = 10;
+	private int maxY = 10;
 	private int shipType;
 	private int positioning;
 	private int amountOfShips = 5;
+	
+	public Spielfeld(){//default rules
+		this.shipType = 0;
+		this.positioning = 0;
+	}
 	
 	public Spielfeld(int shipType, int positioning){
 		this.shipType = shipType;
 		this.positioning = positioning;
 	}
 	
+	public Spielfeld(int positioning){//regular ships, different placement rule
+		this.shipType = 0;
+		this.positioning = positioning;
+	}
+	
 	public int getShipType(){
-		return shipType;
+		return this.shipType;
 	}
 	
 	public int getPositioning(){
-		return positioning;
+		return this.positioning;
 	}
 	
-	public void setShip(int ship, Position startPositon, int direction){
+	public int getTile(Position pos){//please make sure position is in bounds BEFORE calling this method
+		return this.field[pos.getY()][pos.getX()];
+	}
+	
+	public int getTile(int x, int y){//please make sure position is in bounds BEFORE calling this method
+		return this.field[y][x];
+	}
+	
+	public boolean setTile(Position pos, int value){//will check for bounds
+		boolean bounds = checkBounds(pos);
+		if(bounds){
+			this.field[pos.getY()][pos.getX()] = value; 
+		}
+		return bounds;
+	}
+	
+	public boolean setTile(int x, int y, int value){//will check for bounds
+		boolean bounds = checkBounds(x, y);
+		if(bounds){
+			this.field[y][x] = value; 
+		}
+		return bounds;
+	}
+	
+	public boolean checkBounds(Position pos){
+		boolean inside = true;
+		inside &= (pos.getX() > -1);
+		inside &= (pos.getY() > -1);
+		inside &= (pos.getX() < this.maxX);
+		inside &= (pos.getY() < this.maxY);
+		
+		return inside;
+	}
+	
+	public boolean checkBounds(int x, int y){
+		boolean inside = true;
+		inside &= (x > -1);
+		inside &= (y > -1);
+		inside &= (x < this.maxX);
+		inside &= (y < this.maxY);
+		
+		return inside;
+	}
+	
+	public boolean setShip(int ship, Position startPosition, int direction){//TODO unset partial ships
+		boolean set;
+		Position current = startPosition;
 		for(int i = 0; i < ship; i++){
-			if(field[startPositon.getVertical()][startPositon.getHorizontal()] == 0){
-				field[startPositon.getVertical()][startPositon.getHorizontal()] = ship;
+			if(getTile(current) == 0){
+				set = setTile(current, ship);// if set is false, you'll know the placement failed
+				if(!set){
+					return false;
+				}
+			} else{
+				return false;
 			}
 			//to the right
 			if(direction == 1){
-				startPositon.verschiebe(+1, 0);
+				current = current.right();
 			}
 			//to the left
 			else if(direction == -1){
-				startPositon.verschiebe(-1, 0);
+				current = current.left();
 			}
 			//up 
 			else if(direction == -2){
-				startPositon.verschiebe(0, -1);
+				current = current.up();
 			}
 			//down
 			else if(direction == 2){
-				startPositon.verschiebe(0, +1);
+				current = current.down();
 			}    
 		}
+		return true;
 	}
 	
 	public boolean possibleSet(Position pos, int delta){
@@ -147,7 +211,7 @@ public class Spielfeld {
 	}
 	
 	public static void main(String[] args) {
-		Spielfeld test = new Spielfeld(0, 2);
+		Spielfeld test = new Spielfeld(0, 2);		
 		test.setShip(5, new Position(1, 0), 1);
 		test.printField();
 
