89067da2bb4808525f021da76a48cc50e8de6a94
Daniel Korsus <1725757@stud.hs-mannheim.de>
2018-06-24 23:31:03 +0200

    Prepare for sp team

---
+++ SchiffeVersenken/src/se1/schiffeVersenken/interfaces/MyPlayer.java
@@ -0,0 +1,53 @@
+package se1.schiffeVersenken.interfaces;
+
+import se1.schiffeVersenken.interfaces.GameSettings.ShipBorderConditions;
+import se1.schiffeVersenken.interfaces.exception.shipPlacement.InvalidShipPlacementException;
+import se1.schiffeVersenken.interfaces.util.Position;
+
+public class MyPlayer implements Player, ShipPlacer{
+	private int[] numberShips;
+	private ShipBorderConditions border;
+	private AI ai;
+	
+	public MyPlayer(int[] numberShips, ShipBorderConditions border){
+		this.numberShips = numberShips;
+		this.border = border;
+		if(border == ShipBorderConditions.NO_DIRECT_AND_DIAGONAL_TOUCH){
+			ai = new AI(new Spielfeld(0));
+		}
+		else{
+			ai = new AI(new Spielfeld(1));
+		}
+	}
+
+	@Override
+	public void placeShips(ShipPlacer placer) {
+		ai.setShips();
+		
+	}
+
+	@Override
+	public void takeTurn(TurnAction turnAction) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onEnemyShot(Position position, Tile tile, Ship ship) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void gameOver(boolean youHaveWon) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setShips(Ship[] ships) throws InvalidShipPlacementException {
+		
+		
+	}
+
+}
